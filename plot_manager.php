dd0ff2a47924556f7ff49dba5c1e479bee36372e
omariba <omariba5@gmail.com>
2017-01-31 06:48:41 +0300

    Created Metabox Fields

--- plot_manager.php
+++ plot_manager.php
@@ -8,8 +8,10 @@
 	Author URI:nelsomariba.com
 	*/
 
-	if(!defined('ABSPATH')){
-		exit;
+	if ( defined( 'ABSPATH' ) && ! defined( 'RWMB_VER' ) ) {
+		require_once dirname( __FILE__ ) . '/inc/loader.php';
+		$loader = new RWMB_Loader;
+		$loader->init();
 	}
 
 	function plot_manager_post(){
@@ -18,5 +20,38 @@
 	}
 
 	add_action('init','plot_manager_post');
+
+	add_filter( 'rwmb_meta_boxes', 'your_prefix_meta_boxes' );
+    function your_prefix_meta_boxes( $meta_boxes ) {
+        $meta_boxes[] = array(
+            'title'      => __( 'Plot Manager', 'textdomain' ),
+            'post_types' => 'plot_manager',
+            'context'	 => 'advanced',
+            'fields'     => array(
+                array(
+                    'id'   => 'address',
+                    'name' => __( 'Address', 'textdomain' ),
+                    'type' => 'text',
+                ),
+                array(
+                    'id'   => 'size',
+                    'name' => __( 'Size in acres', 'textdomain' ),
+                    'type' => 'text',
+                ),
+                array(
+                    'id'   => 'status',
+                    'name' => __( 'Status', 'textdomain' ),
+                    'type' => 'select',
+                    'options' => array('Developed','Not Developed'),
+                ),
+                array(
+                	'id' => 'button',
+                	'name' => ('Submit'),
+                	'type' => 'button',
+                ),
+            ),
+        );
+        return $meta_boxes;
+    }    
 
 ?>
